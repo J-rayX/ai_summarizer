bf41c47682731d8b80c733daa93a7f9dd240dc69
jkaylight <jkaylight@gmail.com>
2024-06-15 00:15:33 +0100

    demo component refactored

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,77 +1,93 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import { copy, linkIcon, loader, tick } from '../assets';
-import { useLazyGetSummaryQuery } from '../services/article';
-
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
-    url: '',
-    summary: '',
+    url: "",
+    summary: "",
   });
-
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
 
+  // RTK lazy query
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  // Load data from localStorage on mount
   useEffect(() => {
-    const articlesFromLocalStorage =  JSON.parse(
-      localStorage.getItem('articles')
-    )
-    if(articlesFromLocalStorage){
-      setAllArticles(articlesFromLocalStorage)
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
     }
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { data } = await getSummary({ articleUrl: article.url });
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
 
-    if(data?.summary) {
+    if (existingArticle) return setArticle(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
       const updatedAllArticles = [newArticle, ...allArticles];
 
+      // update state and local storage
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
-
-      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
-
-      console.log(newArticle);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
-  }
+  };
+
+  // copy the url and toggle the icon for user feedback
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit(e);
+    }
+  };
 
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
       <div className='flex flex-col w-full gap-2'>
-        <form 
+        <form
           className='relative flex justify-center items-center'
           onSubmit={handleSubmit}
         >
-          <img 
+          <img
             src={linkIcon}
-            alt="link_icon"
+            alt='link-icon'
             className='absolute left-0 my-2 ml-3 w-5'
-           />
+          />
 
-           <input 
+          <input
             type='url'
-            placeholder='Enter a URL'
+            placeholder='Paste the article link'
             value={article.url}
-            onChange={(e) => setArticle({...article, url: e.target.value 
-            })} 
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            onKeyDown={handleKeyDown}
             required
-            className='url_input peer'
-           />
-           <button
-            type='su bmit'
-            className='submit_btn
-              peer-focus:border-gray-700
-              peer-focus:text-gray-700'
-           >
-            ↵
-           </button>
+            className='url_input peer' // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type='submit'
+            className='submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 '
+          >
+            <p>↵</p>
+          </button>
         </form>
 
         {/* Browse History */}
@@ -84,8 +100,8 @@ const Demo = () => {
             >
               <div className='copy_btn' onClick={() => handleCopy(item.url)}>
                 <img
-                  src={copy}
-                  alt={"copy_icon"}
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
                   className='w-[40%] h-[40%] object-contain'
                 />
               </div>
@@ -97,9 +113,9 @@ const Demo = () => {
         </div>
       </div>
 
-      {/* Display Results */}
+      {/* Display Result */}
       <div className='my-10 max-w-full flex justify-center items-center'>
-      {isFetching ? (
+        {isFetching ? (
           <img src={loader} alt='loader' className='w-20 h-20 object-contain' />
         ) : error ? (
           <p className='font-inter font-bold text-black text-center'>
@@ -124,9 +140,8 @@ const Demo = () => {
           )
         )}
       </div>
-      
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
