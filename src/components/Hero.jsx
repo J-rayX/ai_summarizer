027d23c0f35ed857e8a4fdca9d3abb4b67f78c17
jkaylight <jkaylight@gmail.com>
2024-06-15 14:58:58 +0100

    added repo link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
         <button 
-        type='button' onClick={() => window.open('https://github.com/J-rayX')}
+        type='button' onClick={() => window.open('https://github.com/J-rayX/ai_summarizer')}
         className='black_btn'
         >
           GitHub
